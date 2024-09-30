0e2cf36edd698470070abee18e7f8b2a26aaae53
Sam Mulube <sam.mulube@appital.io>
2024-09-30 14:00:29 +0100

    Import resetable once code
    Rather than bring in an extra dependency we just bring the code into the
    project directly

--- session.go
+++ session.go
@@ -22,7 +22,6 @@ import (
 	"sync"
 	"time"
 
-	"github.com/matryer/resync"
 	"github.com/quickfixgo/quickfix/datadictionary"
 	"github.com/quickfixgo/quickfix/internal"
 )
@@ -54,7 +53,7 @@ type session struct {
 	stateTimer *internal.EventTimer
 	peerTimer  *internal.EventTimer
 	sentReset  bool
-	stopOnce   resync.Once
+	stopOnce   internal.Once
 
 	targetDefaultApplVerID string
 
